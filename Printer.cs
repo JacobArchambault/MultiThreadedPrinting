f3f1b0f51119550ab97286ffd94d03a1b23f57ee
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-15 09:27:53 -0400

    Add project files.

---
+++ Printer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreadedPrinting
+{
+    public class Printer
+    {
+        // Lock token.
+        private object threadLock = new object();
+
+        public void PrintNumbers()
+        {
+            lock (threadLock)
+            {
+                // Display Thread info.
+                Console.WriteLine("-> {0} is executing PrintNumbers()",
+                  Thread.CurrentThread.Name);
+
+                // Print out numbers.
+                Console.Write("Your numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    Random r = new Random();
+                    Thread.Sleep(100 * r.Next(5));
+                    Console.Write("{0}, ", i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
